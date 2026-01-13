2d450bbd791a7ba3d1e3f4a9ad289377e597e9fb
Dadens Julianto <dadensjulianto@gmail.com>
2026-01-13 21:33:38 +0700

    update

--- components/sections/business-grid.tsx
+++ components/sections/business-grid.tsx
@@ -81,7 +81,7 @@ export default function BusinessGrid() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {businesses.map((business) => (
           <div
             key={business.id}
